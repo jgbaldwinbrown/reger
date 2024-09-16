6954ccd18d22027cbcb0f3323287fbca2b296367
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2024-09-16 16:53:49 -0600

    added lots of docs

--- reger.go
+++ reger.go
@@ -1,3 +1,9 @@
+// Reger allows you to use any of the typical regexp.Regexp methods on an
+// io.RuneReader or io.Reader. The regexp package only exposes variations on
+// FindReaderIndex for io.RuneReaders. The single new type here, Reger,
+// maintains an internal buffer of all bytes read via reger.Reger.ReadRune,
+// resetting whenever a new regex method is called. This backing buffer is used
+// to build all of the byte slices and strings expected by other regex methods.
 package reger
 
 import (
@@ -7,11 +13,15 @@ import (
 	"bufio"
 )
 
+// The main type here. Holds an io.RuneReader and passes calls to
+// reger.Reger.ReadRune through to it, capturing all read runes in an internal buffer.
 type Reger struct {
 	r io.RuneReader
 	buf []byte
 }
 
+// Reads one rune from Reger's captured io.RuneReader, stores it in the
+// internal buffer, and returns it.
 func (x *Reger) ReadRune() (r rune, size int, err error) {
 	r, size, err = x.r.ReadRune()
 	if err != nil {
@@ -21,14 +31,23 @@ func (x *Reger) ReadRune() (r rune, size int, err error) {
 	return r, size, nil
 }
 
+// Creates a new Reger from an io.RuneReader. No buffering is done, so no extra
+// bytes are read when finding a regex. The reader can still be used by other
+// functions without strange outcomes.
 func NewReger(r io.RuneReader) *Reger {
 	return &Reger{r: r}
 }
 
+// Creates a new Reger from an io.Reader. Internally, a *bufio.Reader is
+// created, and this buffers the input reader. Do not use this reader with
+// other functions.
 func NewReaderReger(r io.Reader) *Reger {
 	return &Reger{r: bufio.NewReader(r)}
 }
 
+// Return a byte slice containing the match, or nil if there is no match.  The
+// backing array for this slice will be overwritten when the next regex method is called and
+// should be copied if you want to use it later.
 func (r *Reger) FindReader(re *regexp.Regexp) []byte {
 	r.buf = r.buf[:0]
 	idxs := re.FindReaderIndex(r)
@@ -38,6 +57,7 @@ func (r *Reger) FindReader(re *regexp.Regexp) []byte {
 	return r.buf[idxs[0]:idxs[1]]
 }
 
+// Return a string containing a match, or "" if there is none.
 func (r *Reger) FindReaderString(re *regexp.Regexp) string {
 	b := r.FindReader(re)
 	if b == nil {
@@ -46,6 +66,9 @@ func (r *Reger) FindReaderString(re *regexp.Regexp) string {
 	return string(b)
 }
 
+// Returns a [][]byte containing all submatches, or nil if there is no match.
+// The backing array for these will be overwritten when the next regex method is called and
+// should be copied if you want to use it later.
 func (r *Reger) FindReaderSubmatch(re *regexp.Regexp) [][]byte {
 	r.buf = r.buf[:0]
 	idxs := re.FindReaderSubmatchIndex(r)
@@ -60,8 +83,12 @@ func (r *Reger) FindReaderSubmatch(re *regexp.Regexp) [][]byte {
 	return out
 }
 
+// Returns a []string containing all submatches or nil if there is no match
 func (r *Reger) FindReaderStringSubmatch(re *regexp.Regexp) []string {
 	bs := r.FindReaderSubmatch(re)
+	if bs == nil {
+		return nil
+	}
 	out := make([]string, 0, len(bs))
 	for _, b := range bs {
 		if b == nil {
@@ -73,6 +100,8 @@ func (r *Reger) FindReaderStringSubmatch(re *regexp.Regexp) []string {
 	return out
 }
 
+// Returns a [][]int containing indices for all matches in the reader or nil if
+// there are no matches.
 func (r *Reger) FindReaderAllIndex(re *regexp.Regexp) [][]int {
 	r.buf = r.buf[:0]
 	var out [][]int
@@ -89,8 +118,14 @@ func (r *Reger) FindReaderAllIndex(re *regexp.Regexp) [][]int {
 	return out
 }
 
+// Returns a [][]byte containing all matches in the reader or nil if there are
+// no matches.  The backing array for these will be overwritten when the next
+// regex method is called and should be copied if you want to use it later.
 func (r *Reger) FindReaderAll(re *regexp.Regexp) [][]byte {
 	idxs := r.FindReaderAllIndex(re)
+	if idxs == nil {
+		return nil
+	}
 	out := make([][]byte, 0, len(idxs))
 	for _, idxpair := range idxs {
 		if idxpair == nil {
@@ -102,8 +137,13 @@ func (r *Reger) FindReaderAll(re *regexp.Regexp) [][]byte {
 	return out
 }
 
+// Returns a []string containing all matches in the reader or nil if there are
+// no matches.
 func (r *Reger) FindReaderAllString(re *regexp.Regexp) []string {
 	bs := r.FindReaderAll(re)
+	if bs == nil {
+		return nil
+	}
 	out := make([]string, 0, len(bs))
 	for _, b := range bs {
 		if b == nil {
@@ -115,6 +155,8 @@ func (r *Reger) FindReaderAllString(re *regexp.Regexp) []string {
 	return out
 }
 
+// Returns a [][]int containing submatch indices for all matches in the reader
+// or nil if there are no matches.
 func (r *Reger) FindReaderAllSubmatchIndex(re *regexp.Regexp) [][]int {
 	r.buf = r.buf[:0]
 	var out [][]int
@@ -132,8 +174,15 @@ func (r *Reger) FindReaderAllSubmatchIndex(re *regexp.Regexp) [][]int {
 	return out
 }
 
+// Returns a [][][]byte containing submatches for all matches in the reader or
+// nil if there are no matches.  The backing array for these will be
+// overwritten when the next regex method is called and should be copied if you
+// want to use it later.
 func (r *Reger) FindReaderAllSubmatch(re *regexp.Regexp) [][][]byte {
 	idxs := r.FindReaderAllSubmatchIndex(re)
+	if idxs == nil {
+		return nil
+	}
 	out := make([][][]byte, 0, len(idxs))
 	for _, idxset := range idxs {
 		if idxset == nil {
@@ -151,6 +200,8 @@ func (r *Reger) FindReaderAllSubmatch(re *regexp.Regexp) [][][]byte {
 	return out
 }
 
+// Returns a [][]string containing submatches for all matches in the reader or
+// nil if there are no matches.
 func (r *Reger) FindReaderAllStringSubmatch(re *regexp.Regexp) [][]string {
 	bsets := r.FindReaderAllSubmatch(re)
 	if bsets == nil {
