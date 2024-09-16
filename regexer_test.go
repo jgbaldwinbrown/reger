91f51bfaa1d3257c8fa4a485b5cd708ac018e7bb
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2024-09-16 16:00:32 -0600

    regexer_test both tests pass

---
+++ regexer_test.go
@@ -0,0 +1,31 @@
+package regexer
+
+import (
+	"testing"
+	"strings"
+	"regexp"
+	"reflect"
+)
+
+const input = "here is some apple cider, and there is an apple spider"
+
+var testRe = regexp.MustCompile(`apple (sp|c)ider`)
+
+func TestFindReaderAllString(t *testing.T) {
+	expected := []string{"apple cider", "apple spider"}
+	found := NewReaderRegexer(strings.NewReader(input)).FindReaderAllString(testRe)
+	if !reflect.DeepEqual(found, expected) {
+		t.Errorf("found %v != expected %v", found, expected)
+	}
+}
+
+func TestFindReaderAllStringSubmatch(t *testing.T) {
+	expected := [][]string{
+		[]string{"apple cider", "c"},
+		[]string{"apple spider", "sp"},
+	}
+	found := NewReaderRegexer(strings.NewReader(input)).FindReaderAllStringSubmatch(testRe)
+	if !reflect.DeepEqual(found, expected) {
+		t.Errorf("found %v != expected %v", found, expected)
+	}
+}
