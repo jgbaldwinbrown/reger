0c6a42edfa1ce75825878cf41c8fd2db06509439
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2024-09-17 11:33:00 -0600

    added submatch in allidx

--- allidx.go
+++ allidx.go
@@ -24,3 +24,24 @@ func FindRuneReaderAllIndex(re *regexp.Regexp, r io.RuneReader) [][]int {
 	}
 	return out
 }
+
+// Returns a [][]int containing indices for all submatches for each match in the reader or nil if
+// there are no matches. Unlike the equivalent method of *reger.Reger, this does not
+// allocate a buffer of bytes.
+func FindRuneReaderAllSubmatchIndex(re *regexp.Regexp, r io.RuneReader) [][]int {
+	curlen := 0
+	var out [][]int
+	for {
+		prevlen := curlen
+		idxs := re.FindReaderSubmatchIndex(r)
+		if idxs == nil {
+			break
+		}
+		curlen += idxs[1]
+		for i, _ := range idxs {
+			idxs[i] += prevlen
+		}
+		out = append(out, idxs)
+	}
+	return out
+}
